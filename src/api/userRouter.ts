8e22aad40db7f0fcdcf2ffdb1494a8460897d607
Jacob <jacobroe@rogers.com>
2025-11-25 15:18:02 -0500

    user routes moved to respective files

---
+++ src/api/userRouter.ts
@@ -0,0 +1,107 @@
+import express from "express";
+
+import {
+    readUser,
+    createUser,
+    updateUser,
+    deleteUser
+} from "../controllers/userController";
+
+const router = express.Router();
+
+
+/**
+ * @openapi
+ * /users/{user_id}:
+ *  get:
+ *    tags:
+ *      - Users
+ *    summary: Get a user by their ID 
+ *    parameters:
+ *      - in: path
+ *        name: user_id 
+ *        required : true
+ *        schema:
+ *          type: integer 
+ *          minimum: 1
+ *        description: The users ID
+ *    responses:
+ *      200:
+ *        description: OK 
+ *        content:
+ *          application/json:
+ *            schema: 
+ *              $ref: '#/components/schemas/user'
+ *      404:
+ *        description: User does not exist
+ *  put:
+ *    tags:
+ *      - Users 
+ *    summary: Override a users data
+ *    parameters:
+ *      - in: path
+ *        name: user_id 
+ *        required : true
+ *        schema:
+ *          type: integer 
+ *          minimum: 1
+ *        description: The users ID
+ *    responses:
+ *      200:
+ *        description: OK 
+ *        content:
+ *          application/json:
+ *            schema: 
+ *              $ref: '#/components/schemas/user'
+ *      404:
+ *        description: User does not exist
+ *  delete:
+ *    tags:
+ *      - Users 
+ *    summary: Delete a user
+ *    parameters:
+ *      - in: path
+ *        name: user_id
+ *        required : true
+ *        schema:
+ *          type: integer 
+ *          minimum: 1
+ *        description: The users ID
+ *    responses:
+ *      204:
+ *        description: User deleted successfully 
+ *      404:
+ *        description: User does not exist
+ */
+router.get("/users/{user_id}", readUser)
+router.put("/users/{user_id}", updateUser)
+router.delete("/users/{user_id}", deleteUser)
+
+/**
+ * @openapi
+ * /users:
+ *  get:
+ *    tags:
+ *      - Users
+ *    summary: Returns a list of all existing users
+ *    responses:
+ *      200:
+ *        description: OK
+ *  post:
+ *    tags:
+ *      - Users
+ *    summary: Add a user 
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/user'
+ *    responses:
+ *      201:
+ *        description: User added successfully
+ *      500:
+ *        description: Something went wrong on our end...
+ */
+router.post("/users")
+router.get("/users")
---
+++ src/controllers/userController.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from "express";
+
+export const readUser = async (req : Request, res : Response) => {
+
+}
+
+export const createUser = async (req : Request, res : Response) => {
+
+}
+
+export const updateUser = async (req : Request, res : Response) => {
+
+}
+
+export const deleteUser = async (req : Request, res : Response) => {
+
+}
+
