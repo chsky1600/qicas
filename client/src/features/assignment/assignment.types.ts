0940ee0890777a9de0098bb451e07127670b5666
Jacob McMullen <jacob.w.mcmullen@gmail.com>
2026-02-20 17:19:05 -0500

    Move state mocks from api to types

--- client/src/features/assignment/assignment.types.ts
+++ client/src/features/assignment/assignment.types.ts
@@ -54,3 +54,98 @@ export const instructorStateEmpty: InstructorState = {
   byId: {},
   allIds: [],
 };
+
+
+export const sectionStateMock: SectionState = {
+  byId: {
+    "0": {
+      id: "0",
+      name: "intro to french (1)",
+      dept: "FREN",
+      code: "101",
+      year_introduced: "xx",
+      section_num: 1,
+      workload: 1,
+      availability: SectionAvailability.F,
+      capacity: 200,
+      assigned_to: "0"
+    },
+    "1": {
+      id: "1",
+      name: "intro to french (2)",
+      dept: "FREN",
+      code: "102",
+      year_introduced: "xx",
+      section_num: 2,
+      workload: 1,
+      availability: SectionAvailability.F,
+      capacity: 200,
+      assigned_to: "0"
+    },
+    "2": {
+      id: "2",
+      name: "intro to french (3)",
+      dept: "FREN",
+      code: "103",
+      year_introduced: "xx",
+      section_num: 3,
+      workload: 1,
+      availability: SectionAvailability.F,
+      capacity: 200,
+      assigned_to: "0"
+    },
+    "3": {
+      id: "3",
+      name: "intro to french (4)",
+      dept: "FREN",
+      code: "104",
+      year_introduced: "xx",
+      section_num: 4,
+      workload: 1,
+      availability: SectionAvailability.F,
+      capacity: 200,
+      assigned_to: "0"
+    },
+    "4": {
+      id: "4",
+      name: "intro to french (5)",
+      dept: "FREN",
+      code: "105",
+      year_introduced: "xx",
+      section_num: 5,
+      workload: 1,
+      availability: SectionAvailability.FandW,
+      capacity: 200,
+      assigned_to: "0"
+    },
+  },
+  allIds: ["0", "1", "2", "3", "4"],
+};
+
+export const instructorStateMock: InstructorState = {
+  byId: {
+    "0": {
+      id: "0",
+      name: "John Robbin",
+      position: {short: "Prof.", long: "Professor"},
+      email: "jr@queensu.ca",
+      workload_total: 4,
+      modifier: 0,
+      notes: "",
+      fall_assigned: new Set<string>(["0", "1", "2", "3","4"]),
+      wint_assigned: new Set<string>(["4"]),
+    },
+    "1": {
+      id: "1",
+      name: "Erin Erika",
+      position: {short: "T.F.", long: "Teaching Fellow"},
+      email: "ee@queensu.ca",
+      workload_total: 2,
+      modifier: 0,
+      notes: "",
+      fall_assigned: new Set<string>(),
+      wint_assigned: new Set<string>(),
+    },
+  },
+  allIds: ["0", "1"],
+};
