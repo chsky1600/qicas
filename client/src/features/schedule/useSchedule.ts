3c737f87df7ffac6e6d7c0dc1d052b10640031be
Jacob <jacobroe@rogers.com>
2026-03-29 17:56:32 -0400

    Updated export date formatting

--- client/src/features/schedule/useSchedule.ts
+++ client/src/features/schedule/useSchedule.ts
@@ -513,8 +513,8 @@ export function useSchedule(): UseScheduleResult {
       now.getFullYear(),
       String(now.getMonth() + 1).padStart(2, "0"),
       String(now.getDate()).padStart(2, "0"),
-      String(now.getHours()).padStart(2, "0"),
-      String(now.getMinutes()).padStart(2, "0"),
+      String(now.getHours()).padStart(2, "0")+"h",
+      String(now.getMinutes()).padStart(2, "0")+"m",
     ].join("-");
   
     // there must be at least 1
